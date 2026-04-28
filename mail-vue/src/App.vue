01555d8e71b7b563f46111b2321666ad3974cf35
frannaa <franna@elektrine.com>
2026-04-28 17:52:06 +0800

    Update App.vue

--- mail-vue/src/App.vue
+++ mail-vue/src/App.vue
@@ -3,14 +3,22 @@
     <router-view />
   </el-config-provider>
 </template>
+
 <script setup>
 import { useI18n } from "vue-i18n";
 import { watch } from "vue";
-import {useSettingStore} from "@/store/setting.js";
+import { useSettingStore } from "@/store/setting.js";
 const settingStore = useSettingStore()
 import zhCn from 'element-plus/es/locale/lang/zh-cn';
 import('@/icons/index.js')
+
 const { locale } = useI18n()
-locale.value = settingStore.lang
-watch(() => settingStore.lang, () => locale.value = settingStore.lang)
+
+// ★ 修改点 1：初始化语言时使用 localStorage，而不是 store
+locale.value = localStorage.getItem("lang") || "en"
+
+// ★ 修改点 2：监听 store.lang，让 UI 自动切换语言（无需刷新）
+watch(() => settingStore.lang, (val) => {
+  locale.value = val
+})
 </script>
