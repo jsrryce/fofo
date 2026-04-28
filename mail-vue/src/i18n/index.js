65cc022839db74f243140fbac1147a779f9c2e6a
frannaa <franna@elektrine.com>
2026-04-28 10:48:08 +0800

    Update index.js

--- mail-vue/src/i18n/index.js
+++ mail-vue/src/i18n/index.js
@@ -1,8 +1,16 @@
 import { createI18n } from 'vue-i18n';
 import en from './en.js'
 import zh from './zh.js'
+
+// ★ 新增：从 localStorage 读取语言（默认 zh）
+const savedLang = localStorage.getItem("lang") || "zh";
+
 const i18n = createI18n({
     legacy: false,
+
+    // ★ 修改：使用 savedLang 作为当前语言
+    locale: savedLang,
+
     messages: {
         zh,
         en
